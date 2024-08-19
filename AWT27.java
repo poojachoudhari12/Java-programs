d4b2e9853ae3ea01ae04638e6f9f55fe5170e7ac
poojachoudhari12 <poojachoudhari775@gmail.com>
2024-08-19 10:34:13 -0700

    Add files via upload

---
+++ AWT27.java
@@ -0,0 +1,106 @@
+import java.awt.*;
+import java.awt.event.*;
+
+public class AWT27 extends Frame implements ActionListener {
+    TextField t1;
+    Panel p1;
+    String[] cap = {"1", "2", "3", "+",
+                    "4", "5", "6", "-",
+                    "7", "8", "9", "*",
+                    "C", "0", "=", "/"};
+    Button[] btn;
+    int v1, v2, res, flg;
+    char ch, opr;
+    String s = "";  // Initialize s to avoid null pointer exception
+
+    AWT27() {
+        super("Calci");
+        t1 = new TextField(15);
+        t1.setEditable(false);
+        add(t1, BorderLayout.NORTH);
+        btn = new Button[16];
+        p1 = new Panel();
+        p1.setLayout(new GridLayout(4, 4, 5, 5));
+        for (int i = 0; i < 16; i++) {
+            btn[i] = new Button(cap[i]);
+            btn[i].addActionListener(this);
+            p1.add(btn[i]);
+        }
+        add(p1, BorderLayout.CENTER);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent we) {
+                System.exit(0);
+            }
+        });
+        setSize(400, 300);
+        setVisible(true);
+    }
+
+    void comp() {
+        switch (opr) {
+            case '+':
+                res = v1 + v2;
+                break;
+            case '-':
+                res = v1 - v2;
+                break;
+            case '*':
+                res = v1 * v2;
+                break;
+            case '/':
+                res = v1 / v2;
+                break;
+        }
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        ch = (e.getActionCommand()).charAt(0);
+        if (ch == 'C') {
+            s = "";
+            t1.setText("");
+            flg = 0;
+            return;
+        }
+        if (ch == '=') {
+            if (flg == 1) {
+                try {
+                    v2 = Integer.parseInt(t1.getText());
+                } catch (Exception e1) {
+                }
+                comp();
+                t1.setText(" " + res);
+                flg = 0;
+                s = "";
+                return;
+            }
+        }
+        if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+            if (flg == 0) {
+                try {
+                    v1 = Integer.parseInt(t1.getText());
+                } catch (Exception e1) {
+                }
+                opr = ch;
+                s = "";
+                flg = 1;
+            } else {
+                try {
+                    v2 = Integer.parseInt(t1.getText());  // Fixed typo from getTexxt() to getText()
+                } catch (Exception e1) {
+                }
+                comp();
+                t1.setText("" + res);
+                v1 = res;
+                opr = ch;
+                s = "";
+            }
+            return;
+        }
+        s = s + ch;
+        t1.setText(s);
+    }
+
+    public static void main(String[] args) {
+        new AWT27();
+    }
+}
